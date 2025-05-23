e389502d433e541b186818c17504318b6dc3d8e6
Aldo Fuster Turpin <aldo@timescale.com>
2025-05-23 15:32:00 +0200

    keep 2 color schemes in colorscheme.lua  and remove unused plugins.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,10 +5,23 @@ return {
   { "ellisonleao/gruvbox.nvim" },
 
   -- Configure LazyVim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "gruvbox", -- https://github.com/ellisonleao/gruvbox.nvim
+  --   },
+  -- },
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox", -- https://github.com/ellisonleao/gruvbox.nvim
-    },
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        -- we can add more options here
+      })
+
+      vim.cmd("colorscheme github_light")
+    end,
   },
 }
